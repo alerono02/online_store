e0df24473f143378e5c393e06d1e5cbe8ba1a1c5
Aleksandr <ribakov.alex2013@yandex.ru>
2023-12-07 13:49:33 +0300

    Добавление фото

--- catalog/models.py
+++ catalog/models.py
@@ -6,7 +6,7 @@ NULLABLE = {'null': True, 'blank': True}
 
 class Category(models.Model):
     name = models.CharField(max_length=150, verbose_name='Категория')
-    description = models.CharField(max_length=150, verbose_name='Описание', **NULLABLE)
+    description = models.CharField(max_length=500, verbose_name='Описание', **NULLABLE)
 
     def __str__(self):
         return f'{self.name}'
@@ -18,9 +18,9 @@ class Category(models.Model):
 
 class Product(models.Model):
     name = models.CharField(max_length=150, verbose_name='Наименование')
-    description = models.CharField(max_length=150, verbose_name='Описание', **NULLABLE)
+    description = models.CharField(max_length=500, verbose_name='Описание', **NULLABLE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
-    image = models.ImageField(upload_to='products/', verbose_name='', **NULLABLE)
+    image = models.ImageField(upload_to='products/', verbose_name='Изображение', **NULLABLE)
     price = models.IntegerField(verbose_name='Цена')
     data_created = models.DateField(verbose_name='Дата создания', auto_now=True)
     data_changed = models.DateField(verbose_name='Дата последнего изменения', auto_now_add=True)
