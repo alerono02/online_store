d0b5c52b16d1f2f7bc56b7496fe9876408ad271d
Aleksandr <ribakov.alex2013@yandex.ru>
2023-12-07 13:49:33 +0300

    edit tags

--- config/settings.py
+++ config/settings.py
@@ -16,8 +16,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-p^0pklnsrdgnu30_1(^%_zldc+2+im_-96r_!v4gpu!#hlj%i0'
